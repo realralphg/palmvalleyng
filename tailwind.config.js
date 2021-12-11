1906ca473e5509d2cadbf99f9bd94cac689652c0
Didier Senou <didiersenou94@gmail.com>
2021-12-11 21:36:33 +0100

    made some config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,25 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
-
+            colors: {
+                green: "#448B42",
+                yellow: "#FFD600",
+                grey: {
+                    DEFAULT: "#ADB9C7",
+                    50: "#FFFFFF",
+                    100: "#FFFFFF",
+                    200: "#FFFFFF",
+                    300: "#EAEDF0",
+                    400: "#CBD3DC",
+                    500: "#ADB9C7",
+                    600: "#8F9FB2",
+                    700: "#70859E",
+                    800: "#596C82",
+                    900: "#445364",
+                },
+            }
         },
-        colors: {
-            green: "#448B42",
-            yellow: "#FFD600"
-        }
+
     },
     variants: {
         extend: {},
