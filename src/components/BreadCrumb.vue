410d0032acca0c0fc9b01e0b0539732a2423ee46
Isah Raphael <33344810+realralphg@users.noreply.github.com>
2022-04-10 15:34:24 +0100

    Space correction

--- src/components/BreadCrumb.vue
+++ src/components/BreadCrumb.vue
@@ -1,42 +1,53 @@
 <template>
-    <div class="breadcrumb flex justify-center items-center flex-col text-center">
-        <p class="title font-bold poppins text-white text-4xl mb-4">{{title}}</p>
-        <p class="description text-white text-xl">{{desc}}</p>
-    </div>
+  <div class="breadcrumb flex justify-center items-center flex-col text-center">
+    <p class="title font-bold poppins text-white text-4xl mb-4">{{ title }}</p>
+    <p class="description text-white text-xl">{{ desc }}</p>
+  </div>
 </template>
 
 <script setup>
-import {useRoute} from 'vue-router'
-import {computed} from 'vue'
+import { useRoute } from "vue-router";
+import { computed } from "vue";
 const props = defineProps({
-    title: {
-        type: String,
-        default: 'route.name'
-    },
-    desc: {
-        type: String,
-        default: ''
-    }
-})
-const data = [{
-    title: 'About Us',
-    description: 'Providing agricultural solutions to an oil dependent Nigeria.'
-},{
-    title: 'Our Businesses',
-    description: 'Palm Valley Nigeria is a household name in Agribusiness Support Service provision in Nigeria'
-}, {
-    title: 'Our core Activities',
-    description: 'Providing agricultural solutions to an oil dependent Nigeria.'
-},{
-    title: 'Partnership with Agribusiness Clusters',
-    description: 'Providing business support services to the following agribusiness clusters and Cooperative associations.'
-}, {
-    title: 'Our Businesses',
-    description: 'Palm Valley Nigeria is a household name in Agribusiness Support Service provision in Nigeria'
-}]
+  title: {
+    type: String,
+    default: "route.name",
+  },
+  desc: {
+    type: String,
+    default: "",
+  },
+});
+const data = [
+  {
+    title: "About Us",
+    description:
+      "Providing agricultural solutions to an oil dependent Nigeria.",
+  },
+  {
+    title: "Our Businesses",
+    description:
+      "Palm Valley Nigeria is a household name in Agribusiness Support Service provision in Nigeria",
+  },
+  {
+    title: "Our core Activities",
+    description:
+      "Providing agricultural solutions to an oil dependent Nigeria.",
+  },
+  {
+    title: "Partnership with Agribusiness Clusters",
+    description:
+      "Providing business support services to the following agribusiness clusters and Cooperative associations.",
+  },
+  {
+    title: "Our Businesses",
+    description:
+      "Palm Valley Nigeria is a household name in Agribusiness Support Service provision in Nigeria",
+  },
+];
 // const route = useRoute()
 // const selectBreadCrumb = computed(() => {
-//     
+//
 //     let obj;
 //     console.log(route.path)
 //     switch(route.path) {
@@ -61,6 +72,4 @@ const data = [{
 // selectBreadCrumb()
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
